2ad537559efd251404584f3dd19517222101a045
Carles Núñez Tomeo <carlesnunezt@gmail.com>
2020-05-22 19:59:26 +0200

    First commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,60 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import fetch from 'node-fetch'
+import * as github from '@actions/github'
+
+function getInputs(): {repoToken: string; giphyToken: string} {
+  const repoToken: string = core.getInput('repo-token')
+  const giphyToken: string = core.getInput('giphy-token')
+
+  return {
+    repoToken,
+    giphyToken
+  }
+}
+
+async function getGifUrl(token: string): Promise<string> {
+  const res = await fetch(
+    `http://api.giphy.com/v1/gifs/random?api_key=${token}`
+  )
+
+  const body = await res.json()
+
+  return body.data.image_url
+}
+
+async function pushComment(repoToken: string, comment: string): Promise<void> {
+  const {
+    payload: {pull_request: pullRequest, repository}
+  } = github.context
+
+  const repoFullName = repository?.full_name
+
+  if (!pullRequest || !repoFullName) {
+    core.error('this action only owrks on pull_request events')
+    core.setOutput('comment-created', 'false')
+  } else {
+    const {number: issueNumber} = pullRequest
+    const [owner, repo] = repoFullName.split('/')
+
+    const octokit = new github.GitHub(repoToken)
+
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: comment
+    })
+  }
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug('Initializing the github action')
+    const {repoToken, giphyToken} = getInputs()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const randomGif = await getGifUrl(giphyToken)
 
-    core.setOutput('time', new Date().toTimeString())
+    await pushComment(repoToken, `![](${randomGif})`)
   } catch (error) {
     core.setFailed(error.message)
   }
